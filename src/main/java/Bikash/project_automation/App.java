dff782b996b2a30557a4e357d094027fc04abda0
SagarBikash <bikashkumarswain1@gmail.com>
2024-04-06 22:50:48 +0530

    my first project

---
+++ src/main/java/Bikash/project_automation/App.java
@@ -0,0 +1,13 @@
+package Bikash.project_automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
